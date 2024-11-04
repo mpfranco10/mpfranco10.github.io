03e81d31355c0c517f0797bcaf4888f4b91feb91
María Franco <maria.franco@takecommandhealth.com>
2024-11-04 17:08:50 +0900

    hotjar script

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import useHotjar from "react-use-hotjar";
-
-const myCustomLogger = console.info;
 
 function App() {
   const [count, setCount] = useState(0);
